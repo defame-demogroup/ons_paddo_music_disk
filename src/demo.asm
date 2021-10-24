19029693d9785df237b088b0461bb34a03818c5c
Z <joe@pixolut.com>
2021-10-24 15:17:19 +1100

    spinner git add *!

--- src/demo.asm
+++ src/demo.asm
@@ -25,7 +25,6 @@ loader_init:
 .var music_play = $0f04
 
 .pc=$3000 "PETSCII Animation Buffers"
-.import source "spinner_include.asm"
 
 .pc=$5000 "Code"
 .pc=* "Exomizer"
@@ -258,6 +257,9 @@ press_space:
 enable_music:
 .byte $00
 
+enable_effect:
+.byte $00
+
 /*
 -----------------
 Interrupt Handler
@@ -265,6 +267,15 @@ Interrupt Handler
 */
 .pc=* "irq"
 //fancy intro IRQ!
+irq_loader:
+    lda enable_effect
+    beq !+
+    jsr spinner_run
+!:
+    lda #$ff 
+    sta $d019
+    jmp $ea31  
+
 irq_intro:
     jsr s_scroll
     lda #$ff 
@@ -360,6 +371,49 @@ m_play:
 !skip:
     rts
 
+.align $100
 m_shadow:
     .fill $100, $00
 
+.pc = * "spinner"
+.align $100
+.import source "spinner_include.asm"
+
+spinner_reset_txt:
+.byte >spinner1_txt
+spinner_reset_col:
+.byte >spinner1_col
+spinner_current:
+.byte $00
+spinner_delay:
+.byte $02
+
+spinner_run:
+    dec spinner_delay
+    beq !+
+    rts
+!:  lda #$02
+    sta spinner_delay
+    lda spinner_current
+    cmp #$08
+    bne !no_reset+
+    lda #$00
+    sta spinner_current
+    lda spinner_reset_txt
+    sta spinner_txt_ptr + 1
+    lda spinner_reset_col
+    sta spinner_col_ptr + 1
+!no_reset:
+    ldx #$00
+!:
+    lda spinner_txt_ptr: spinner1_txt,x
+    sta ($0400 + ($28 * 8)),x
+    lda spinner_col_ptr: spinner1_col,x
+    sta ($d800 + ($28 * 8)),x
+    inx
+    cpx #(40 * 6)
+    bne !-
+    inc spinner_current
+    inc spinner_col_ptr + 1
+    inc spinner_txt_ptr + 1
+    rts
